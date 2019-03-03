64782b5b1879cf189d37486807ff63b0b0df2973
LiangCGL <48084168+LiangCGL@users.noreply.github.com>
2019-03-03 15:24:15 +0800

    Add files via upload

---
+++ Lqueue.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+
+class linkqueuenode
+{
+public:
+	linkqueuenode()
+	{
+		_next = NULL;
+	}
+	~linkqueuenode();
+
+private:
+	int _data;
+	linkqueuenode * _next;
+
+	friend class linkqueue;
+
+};
+
+
+class linkqueue
+{
+public:
+	linkqueue()
+		:_head()
+	{
+		_count = 0;
+	}
+
+	linkqueuenode * BuyNode(int  val, linkqueuenode *next)
+	{
+		linkqueuenode *s = new linkqueuenode;
+		assert(s != NULL);
+
+		s->_data = val;
+		s->_next = next;
+
+		return s;
+	}
+
+	void Push(int data)
+	{
+		linkqueuenode *s = linkqueue::BuyNode(data, _head._next);
+		s->_next = _head._next;
+		_head._next = s;
+		_count++;
+	}
+
+	void Pop()
+	{
+		linkqueuenode *s = _head._next;
+		for (int i = 0; i < _count - 2; i++)
+		{
+			s = s->_next;
+		}
+		linkqueuenode *p = s->_next;
+		s->_next = NULL;
+		delete p;
+		_count--;
+	}
+
+
+	linkqueue(const linkqueue &src)
+	{
+		_head._next = src._head._next;
+		for (int i = 0; i < _count; i++)
+		{
+			Push(src._head._data);
+		}
+	}
+
+	linkqueue operator = (const linkqueue & src)
+	{
+		if (this == &src)
+		{
+			return *this;
+		}
+		if (NULL != _head._next)
+		{
+			delete _head._next;
+		}
+		_head._next = src._head._next;
+		for (int i = 0; i < _count; i++)
+		{
+			Push(src._head._data);
+		}
+
+		return *this;
+	}
+
+	~linkqueue();
+private:
+	linkqueuenode _head;
+	int _count;
+
+};
---
+++ Lstack.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+class linkstacknode
+{
+public:
+	linkstacknode()
+	{
+		_next = NULL;
+	}
+	~linkstacknode();
+
+private:
+	int _data;
+	linkstacknode * _next;
+
+	friend class linkstack;	
+
+};
+
+
+class linkstack
+{
+public:
+	linkstack()
+		:_top()
+	{
+		_count = 0;
+	}
+
+	linkstacknode * BuyNode(int  val, linkstacknode *next)
+	{
+		linkstacknode *s = new linkstacknode;
+		assert(s != NULL);
+
+		s->_data = val;
+		s->_next = next;
+
+		return s;
+	}
+
+	void Push(int data)
+	{
+		linkstacknode *s = linkstack::BuyNode(data, _top._next);
+		s->_next = _top._next;
+		_top._next = s;
+		_count++;
+
+	}
+
+	void Pop()
+	{
+		linkstacknode *s = _top._next;
+		_top._next = s->_next;
+		delete s;
+		_count--;
+	}
+
+	linkstack(const linkstack & src)
+	{ 
+		_top._next = src._top._next;
+		for (int i = 0; i < _count; i++)
+		{
+			Push(src._top._data);
+		}
+	}
+
+	linkstack operator = (const linkstack & src)
+	{
+		if (this == &src)
+		{
+			return *this;
+		}
+
+		if (NULL != _top._next)
+		{
+			delete _top._next;
+		}
+		_top._next = src._top._next;
+		for (int i = 0; i < _count; i++)
+		{
+			Push(src._top._data);
+		}
+
+		return *this;
+	}
+
+
+	~linkstack();
+	
+private:
+	linkstacknode _top;
+	int _count;
+	
+};
+
+
+
+int main()
+{
+
+	return 0;
+}
